7b253a8335c29ce02cca44ebec4be22865757654
shin <t_shinya4@yahoo.co.jp>
2011-07-06 14:24:55 +0900

    /search?q= 	redirect list

--- grails-app/controllers/snippet/SnippetController.groovy
+++ grails-app/controllers/snippet/SnippetController.groovy
@@ -100,9 +100,14 @@ class SnippetController {
     
     def search = {
     	//render Snippet.search(params.q, params)
-    	def searchResults = Snippet.search(params.q, params)
-    	flash.message = "${searchResults.total} results foundo for search: ${params.q}"
-    	flash.q = params.q
-    	[searchResults:searchResults.results, resultCount:searchResults.total]
+    	if (params.q){
+	    	def searchResults = Snippet.search(params.q, params)
+	    	flash.message = "${searchResults.total} results foundo for search: ${params.q}"
+	    	flash.q = params.q
+	    	[searchResults:searchResults.results, resultCount:searchResults.total]
+	    }
+	    else {
+	    	redirect(action: "list")
+	    }
     }
 }

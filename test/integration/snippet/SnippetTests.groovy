7755dfb90b0affcdd589462529f1260e50d4f1f5
shin <t_shinya4@yahoo.co.jp>
2011-07-20 15:24:29 +0900

    Test

--- test/integration/snippet/SnippetTests.groovy
+++ test/integration/snippet/SnippetTests.groovy
@@ -16,12 +16,6 @@ class SnippetTests extends GroovyTestCase {
 
     void testSomething() {
 
-        // mockDomain(Snippet)
-        // mockDomain(Patch)
-        // mockDomain(UserRole)
-        // mockDomain(User)
-        // mockDomain(Role)
-
         def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
         def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
         def testUser1 = new User(username: 'admin', enabled: true, password: 'password')
@@ -39,10 +33,5 @@ class SnippetTests extends GroovyTestCase {
         def snippet = new Snippet(author:User.get(2),name:"Test",snippet:"main()\n{\n\treturn 0;\n}\n")
         snippet.save(flush: true)
         assert Snippet.count() == 1
-
-        def sc = new SnippetController()
-        sc.params.id=1
-        sc.params.snippet="#include <stdio.h>\nmain()\n{\n}\n"
-        sc.update()
     }
 }

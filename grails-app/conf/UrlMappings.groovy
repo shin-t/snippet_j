d786e2871ab239044f9d7e890633ad6a1415ea0c
shin <t_shinya4@yahoo.co.jp>
2011-10-12 13:28:04 +0900

    url mappings / snippet/index

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,16 +1,16 @@
 class UrlMappings {
 
     static mappings = {
-        "/login"(controller:"login", action:"auth")
-        "/logout"(controller:"logout", action:"index")
-        "/signup"(controller:"user", action:"create")
+        '/login'(controller:'login', action:'auth')
+        '/logout'(controller:'logout', action:'index')
+        '/signup'(controller:'user', action:'create')
         "/$controller/$action?/$id?"{
             constraints {
                 // apply constraints here
             }
         }
 
-        "/"(view:'/index')
-        "500"(view:'/error')
+        '/'(controller:'snippet', action:'index')
+        '500'(view:'/error')
     }
 }

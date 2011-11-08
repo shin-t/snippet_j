fd5aee6bcdcb2c7914dd84d59e79dac06ebda819
shin <t_shinya4@yahoo.co.jp>
2011-11-08 15:10:29 +0900

    Bootstrap (resources)

--- grails-app/conf/AppResources.groovy
+++ grails-app/conf/AppResources.groovy
@@ -1,9 +1,14 @@
 modules = {
     common {
-        dependsOn 'jquery-ui, jquery'
+        dependsOn 'jquery'
         resource url:'css/main.css'
         resource url:'google-code-prettify/prettify.css'
         resource url:'google-code-prettify/prettify.js'
         resource url:'js/common.js'
+    }
+    bootstrap {
+        dependsOn 'jquery'
+        resource url:'bootstrap/bootstrap.min.css'
+        resource url:'bootstrap/bootstrap-buttons.js'
     }
 }
